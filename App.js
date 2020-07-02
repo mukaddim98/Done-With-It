e5b976e7c6f63c8dd5983b890571966024321c1c
mukaddim98 <mukaddim1998@gmail.com>
2020-07-02 17:40:05 -0300

    Request permission feature working

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect } from "react";
+import Screen from "./app/components/Screen.js";
+import * as ImagePicker from "expo-image-picker";
+import { Text } from "react-native";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return (
+    <Screen>
+      <Text>Working!</Text>
+    </Screen>
+  );
 }
