6b9d79664c89be374e8b0d316c70889c45e9ebeb
mukaddim98 <mukaddim1998@gmail.com>
2020-07-07 19:34:43 -0300

    Stack navigator working

--- App.js
+++ App.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Text, Button } from "react-native";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
+import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title="Click"
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>TweetDetails {route.params.id}</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={{ headerTitle: "Hello?" }}
+    />
+  </Stack.Navigator>
+);
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
